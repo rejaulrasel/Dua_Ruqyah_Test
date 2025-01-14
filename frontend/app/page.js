834ee76f8b5f8563fa656586d1a756c907a69963
rejaulrasel <rejaulhasanrasel28@gmail.com>
2025-01-14 10:48:49 +0600

    initialize basic frontend setup

---
+++ frontend/app/page.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-5xl text-center">This is Dua Ruqyah page</h1>
+    </div>
+  );
+}
